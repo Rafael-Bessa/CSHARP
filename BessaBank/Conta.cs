a9c9a3f92beebcd8e1af5693287a099a2caee88e
Rafael Bessa <rafaelbessacivil@gmail.com>
2024-02-25 20:17:04 -0300

    Testando Classe Conta e criando objetos no main

---
+++ BessaBank/Conta.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BessaBank
+{
+    public class Conta
+    {
+
+        private String titular;
+        private Double saldo;
+
+        public Conta(double saldo, string nome)
+        {
+            this.titular = nome;
+            this.saldo = saldo;
+        }
+
+        public string Titular { get => titular; set => titular = value; }
+        public double Saldo { get => saldo; set => saldo = value; }
+
+        public void Deposita(double valor) 
+        {
+            this.saldo += valor;
+        }
+
+        public void Saca(double valor)
+        {
+            this.saldo -= valor;
+        }
+
+        public void Transfere(double valor, Conta conta)
+        {
+            this.saldo -= valor;
+            conta.Saldo += valor;
+        }
+        public override string ToString()
+        {
+            return $"Nome: {Titular}, Saldo da Conta: {Saldo}";
+        }
+    }
+}
---
+++ BessaBank/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using BessaBank;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        Conta conta1 = new Conta (0.0, "Rafael");
+        Conta conta2 = new Conta(200.0, "Bessa");
+        Console.WriteLine(conta1.ToString()); //0
+
+        conta1.Deposita(100.0);
+        conta1.Saca(50.0);
+        Console.WriteLine(conta1.ToString()); //50
+        conta2.Transfere(50.0, conta1);
+        Console.WriteLine("***********************************************************************");
+        Console.WriteLine(conta1.ToString()); //100
+        Console.WriteLine(conta2.ToString()); // 150
+
+    }
+}
